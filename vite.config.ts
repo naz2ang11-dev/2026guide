fd359d7099e131fb0cc907d08e68c5ab096b4b2d
naz2ang11-dev <naz2ang11@gmail.com>
2026-02-11 18:21:06 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,10 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/2026guide/', // 이 부분을 추가하세요!
+})
