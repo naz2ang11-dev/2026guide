2cdbfab89f7be9371cd196477b689f22d641660e
naz2ang11-dev <naz2ang11@gmail.com>
2026-02-11 18:33:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,15 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+// 설정을 하나로 합쳤습니다!
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/2026guide/', // 이 부분을 추가하세요!
-})
+  base: '/2026guide/', // 깃허브 페이지 주소를 위한 설정
+}))import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// 설정을 하나로 합쳤습니다!
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  base: '/2026guide/', // 깃허브 페이지 주소를 위한 설정
+}))
